7ff6f395e891523db47fac09ee3108f5aaef4daa
Tanin Rojanapiansatith <sp_land@hotmail.com>
2018-07-11 14:48:07 +0100

    Add fragment on top of activity and add expandable RV on it

--- app/src/main/java/com/landtanin/expandabledraggableswipeabletest/MainActivity.java
+++ app/src/main/java/com/landtanin/expandabledraggableswipeabletest/MainActivity.java
@@ -2,36 +2,19 @@ package com.landtanin.expandabledraggableswipeabletest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.landtanin.expandabledraggableswipeabletest.utils.ActivityUtils;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
+
 //    private List<StupidModel> models = new ArrayList<>();
-    private List<StupidGroupModel> groupModels = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-
-        for (int groupIndex = 0; groupIndex < 15; groupIndex++) {
-
-            StupidGroupModel group = new StupidGroupModel(
-                    groupIndex,
-                    "group " + groupIndex);
-            for (int i = 0; i < 5; i++) {
-                group.children.add(new StupidModel(i, "name " + i));
-            }
-
-            groupModels.add(group);
-        }
 
         // Swipeable RV
 //        recyclerView = findViewById(R.id.rv);
@@ -84,7 +67,12 @@ public class MainActivity extends AppCompatActivity {
 //
 //        expandableItemManager.attachRecyclerView(recyclerView);
 
-
+        MainFragment mainFragment = new MainFragment();
+        ActivityUtils.addFragmentToActivity(
+                getSupportFragmentManager(),
+                mainFragment,
+                R.id.main_content_container
+        );
 
     }
 }
--- app/src/main/java/com/landtanin/expandabledraggableswipeabletest/MainFragment.java
+++ app/src/main/java/com/landtanin/expandabledraggableswipeabletest/MainFragment.java
@@ -3,14 +3,25 @@ package com.landtanin.expandabledraggableswipeabletest;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SimpleItemAnimator;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.h6ah4i.android.widget.advrecyclerview.expandable.RecyclerViewExpandableItemManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by landtanin on 5/1/2017 AD.
  */
 public class MainFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private List<StupidGroupModel> groupModels = new ArrayList<>();
 
     // constructor of Fragment must be Default constructor (has no arguments)
     public MainFragment() {
@@ -46,7 +57,7 @@ public class MainFragment extends Fragment {
 //        In case of using binding tool
 //        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_habit_frequency, container, false);
 //        View rootView = binding.getRoot();
-        View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         initInstances(rootView, savedInstanceState);
 //        binding.setViewModel(viewModel);
         return rootView;
@@ -62,6 +73,38 @@ public class MainFragment extends Fragment {
         // Init 'View' instance(s) with rootView.findViewById here
         // Note: State of variable initialized here could not be saved
         //       in onSavedInstanceState
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+
+        for (int groupIndex = 0; groupIndex < 15; groupIndex++) {
+
+            StupidGroupModel group = new StupidGroupModel(
+                    groupIndex,
+                    "group " + groupIndex);
+            for (int i = 0; i < 5; i++) {
+                group.children.add(new StupidModel(i, "name " + i));
+            }
+
+            groupModels.add(group);
+        }
+
+        // Expandable RV
+
+        recyclerView = rootView.findViewById(R.id.rv);
+        RecyclerViewExpandableItemManager expandableItemManager
+                = new RecyclerViewExpandableItemManager(null);
+
+        ExpandableAdapter expandableAdapter = new ExpandableAdapter(getContext(), groupModels);
+        RecyclerView.Adapter wrappedAdapter = expandableItemManager
+                                                .createWrappedAdapter(expandableAdapter);
+
+
+        recyclerView.setAdapter(wrappedAdapter);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setHasFixedSize(true);
+
+        ((SimpleItemAnimator) recyclerView.getItemAnimator()).setSupportsChangeAnimations(false);
+
+        expandableItemManager.attachRecyclerView(recyclerView);
     }
 
     @Override
